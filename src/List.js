20b20cec0e105fef3c222cf798044bc6a8c0a166
virysik <68701496+virysik@users.noreply.github.com>
2020-10-28 14:42:22 +0100

    improved styling

--- src/List.js
+++ src/List.js
@@ -4,20 +4,29 @@ import "./List.css";
 export default function List() {
     return (
         <div className = "List">
-            
-                <div className="row">
-                <div className="col-10">
-                        <span>* ___________</span>
-                        </div>
-                         <div className = "col-2">
-                    <button>NOT DONE</button>
-                    </div>
-                  </div>
+             <div className="row">
+                <div className="col-8">
+                    <ul>
+                        <li>
+                            _________________
+                        </li>
+                   </ul>
+                </div>
+                <div className="col-4">  
+                      <button className = "btn btn-primary">Not done</button>
+                </div> 
+            </div>
                     
             
             <form>
-                <input type="text" placeholder="type an action" />
-                <input type="button" value="Add" />
+                <div className="row"> 
+                  <div className="col-9">  
+                     <input type="text" className = "form-control"  placeholder="type an action" />
+                   </div> 
+                   <div className="col-3">
+                        <input type="button" className="btn btn-primary" value="Add" />
+                   </div>
+                </div>
             </form>
         </div>
 )
